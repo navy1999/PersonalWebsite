15b5de10d26113a319f4275a27491ed381a818c0
Navneet Shankar <32503754+navy1999@users.noreply.github.com>
2024-07-28 22:47:55 +0530

    Update about.js

--- src/Components/Script/about.js
+++ src/Components/Script/about.js
@@ -7,9 +7,9 @@ const about =() =>{
            <Container>  
             <h3>About Me</h3>
             <div>
-                <p>I am a software developer with over 2 years of experience. My personal interests are programming, deep learning, genomics and history.</p>
+                <p>I am a software developer with 3 years of experience, and currently a graduate student at Virginia Tech, Blacksburg. My personal interests are programming, deep learning, genomics and history.</p>
                 <p>I graduated from National Institute of Technology Karnataka with a BTech in Electronics and Communication Engineering</p>
-                <p>The Tech stack that I currently work with includes Java, Springboot , Scala, Apache Spark, FTL, Apache Hadoop, Hive, Impala, SQL, ReactJS & AngularJS. In addition I have good experience working with C, C++, Python and also dabbled in Kotlin</p>
+                <p>I am currently working on projects in Deep Genomics, Computer Vision, Machine Learning Fundamentals.
             </div>
             </Container> 
         </div>
