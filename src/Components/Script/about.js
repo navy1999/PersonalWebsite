6c2d9f6f5212cbb5cf2ef15d985e648568fcb6e1
Navneet Shankar <navneets099@gmail.com>
2024-08-17 10:34:34 +0530

    Minor correction

--- src/Components/Script/about.js
+++ src/Components/Script/about.js
@@ -18,7 +18,7 @@ const About =() =>{
             <div>
                 <p>I am a software developer with 3 years of experience, and currently a graduate student at Virginia Tech, Blacksburg. My personal interests are programming, deep learning, genomics and history.</p>
                 <p>I graduated from National Institute of Technology Karnataka with a BTech in Electronics and Communication Engineering</p>
-                <p>I am currently working on projects in Deep Genomics, Computer Vision, Machine Learning Fundamentals.
+                <p>I am currently working on projects in Deep Genomics, Computer Vision, Machine Learning Fundamentals.</p>
             </div>
             </Container> 
             <Container>
